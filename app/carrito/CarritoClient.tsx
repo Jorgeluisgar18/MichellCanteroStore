fe774866e1236d860dd61ddff55dde650f7db17a
Jorge Garcia <jorgeluisgar18@gmail.com>
2026-01-07 16:37:14 -0500

    Fix unused getTax in CarritoClient

--- app/carrito/CarritoClient.tsx
+++ app/carrito/CarritoClient.tsx
@@ -19,7 +19,6 @@ export default function CarritoClient() {
         updateQuantity,
         removeItem,
         getSubtotal,
-        getTax,
         getShipping,
         getTotal,
     } = useCartStore();
